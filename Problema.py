9e7ae0257d9d48cc7490f76c145c855e5ba58b6d
Phlaveeoh <flavio.merli.tv.03@gmail.com>
2025-03-31 15:47:23 +0200

    Aggiunti metodi a Board e tolti import non utilizzati

--- Problema.py
+++ Problema.py
@@ -1,12 +1,7 @@
-from aima import Problem, Node, PriorityQueue, GraphProblem, memoize, breadth_first_graph_search
+from aima import Problem, Node, PriorityQueue, memoize, breadth_first_graph_search
 from collections.abc import Callable
-from collections import deque
-from colorama import Fore, Back, Style
-import time, sys
+import time
 from collections.abc import Callable
-import functools
-import random
-import math
 from copy import deepcopy
 
 BLUE = "\033[34;1m"
@@ -16,11 +11,11 @@ RESET = "\033[0m"
 
 #Metodo per risolvere un problema con un dato algoritmo
 def execute(name: str, algorithm: Callable, problem: Problem, *args) -> None:
-    print(f"{BLUE}{name}{RESET}\n")
+    print(f"{BLUE}{name}{RESET}")
     start = time.time()
     sol = algorithm(problem, *args)
     end = time.time()
-    print(f"\n{GREEN}PROBLEM:{RESET} {problem.initial} -> {problem.goal}")
+    print(f"{GREEN}Problem:{RESET}\n{problem.initial}\n{GREEN}Goal:{RESET}\n{problem.goal}")
     print(f"{GREEN}Result:{RESET} {sol.solution() if sol is not None else '---'}")
     if isinstance(sol, Node):
         print(f"{GREEN}Path Cost:{RESET} {sol.path_cost}")
@@ -63,13 +58,29 @@ def aStar(problema: Problem, h : Callable | None = None) -> Node:
 def ucs(problem: Problem) -> Node:
   return bfss(problem, lambda node : node.path_cost)
 
+
 class Board():
     '''La classe Board rappresenta il dominio del problema, ovvero una griglia 6*6
     nella quale 6 blocchi numerati possono essere disposti uno sopra l'altro'''
-    
-    #Costruttore della classe
+
     def __init__(self, matrix):
         self.matrix = matrix
+
+    def __eq__(self, other):
+        if not isinstance(other, Board):
+            return False
+        return self.matrix == other.matrix
+
+    def __hash__(self):
+        return hash(tuple(tuple(row) for row in self.matrix))
+
+    def __str__(self):
+        ruotata = [list(row) for row in zip(*self.matrix[::-1])]
+        specchiata = [row[::-1] for row in ruotata]
+        return "\n".join([" ".join(f"{cell:2}" for cell in row) for row in specchiata])
+
+    def __repr__(self):
+        return self.__str__()
     
     #Metodo che restituisce gli spostamenti possibili che può fare un dato blocco
     def get_legal_positions(self, x, y):
@@ -83,17 +94,8 @@ class Board():
                     #l'azione effettuabile è rappresentata da una tupla(x, y, nx, ny)
                     # dove la prima coppia di coordinate è la posizione attuale del blocco, la seconda coppia è la nuova posizione dove verrà spostato
                     positions.append((x, y, nx, ny))
-                    print(f"{self.matrix[x][y]} da {x},{y} a {nx},{ny}")
+                    #print(f"{self.matrix[x][y]} da {x},{y} a {nx},{ny}")
         return positions
-
-#Esempio di stato del problema
-#[[0,0,0,1,4,5]]
-#[[0,0,0,0,0,0]]
-#[[0,0,0,0,0,6]]
-#[[0,0,0,0,0,0]]
-#[[0,0,0,0,3,2]]
-#[[0,0,0,0,0,0]]
-
 
 class BlocksWorldProblem(Problem):
     '''Il seguente problema consiste nello spostare i blocchi in uno spazio da una certa
@@ -124,7 +126,7 @@ class BlocksWorldProblem(Problem):
         temp = next_board.matrix[x][y]
         next_board.matrix[x][y] = 0
         next_board.matrix[nx][ny] = temp
-        print(next_board.matrix)
+        #print(next_board.matrix)
         return next_board
 
     #Metodo che controlla se lo stato che riceve come parametro è lo stato goal
@@ -133,7 +135,6 @@ class BlocksWorldProblem(Problem):
         state to self.goal or checks for state in self.goal if it is a
         list, as specified in the constructor. Override this method if
         checking against a single self.goal is not enough."""
-        print("testiamo se abbiamo finito")
         for x in range(len(state.matrix)):
             for y in range(6):
                 if state.matrix[x][y] != self.goal.matrix[x][y]:
@@ -155,7 +156,7 @@ class BlocksWorldProblem(Problem):
 
 #Piccoli test 
 tavola = Board([[0,0,0,1,4,5],[0,0,0,0,0,0],[0,0,0,0,0,6],[0,0,0,0,0,0],[0,0,0,0,3,2],[0,0,0,0,0,0]])
-problema = BlocksWorldProblem(tavola, Board([[0,0,0,1,4,5],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,6,3,2],[0,0,0,0,0,0]]))
+problema = BlocksWorldProblem(tavola, Board([[0,0,0,1,4,5],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,6],[0,0,0,0,0,0],[0,0,0,0,2,3]]))
 problema2 = BlocksWorldProblem(Board([[0,0,0,1,4,5],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,6,3,2],[0,0,0,0,0,0]]), Board([[0,0,0,1,4,5],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,6,3,2],[0,0,0,0,0,0]]))
     
 execute("BFS", breadth_first_graph_search, problema)
