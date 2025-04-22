2d14564573442f2dfabda1198b4efe45a43eeb37
Phlaveeoh <flavio.merli.tv.03@gmail.com>
2025-04-22 11:09:58 +0200

    diocane

--- Riconoscimento.py
+++ Riconoscimento.py
@@ -43,6 +43,23 @@ mlp_combined = MLPClassifier(
 )
 mlp_combined.fit(x_train_norm, y_train)
 
+# CNN model definition
+model = Sequential([
+    Input(shape=(32, 32, 3)),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    #Dropout(0.2),
+    Conv2D(64, (3, 3), activation='relu'),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')  # 10 output class
+])
+
+# Model architecture visualization
+model.summary()
+
 # Valutazione del modello combinato
 predictions = mlp_combined.predict(x_test)
 accuracy = metrics.accuracy_score(y_test, predictions)
