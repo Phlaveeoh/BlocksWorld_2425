a1e6836a9ab6da84b88902153975e78788ebbaf7
Phlaveeoh <flavio.merli.tv.03@gmail.com>
2025-04-30 11:52:31 +0200

    pulita struttura progetto

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import Riconoscimento as ric
 import Problema as problema
 from keras.models import load_model
-## carichiamo le 2 iimmagini
-## funzione riconoscimento.py che di darà la matrice 6x6
-## salviamo le matrici
-## le diamo in pasto a problema.py
-## creiamo le gif 
-pathImgInput = ".\\test_immagini\\scenaTelefono3.jpg"
-pathImgOutput = ".\\test_immagini\\scenaTelefono5.jpg"
-pathModello = ".\\modelloDenso.keras"
 
+pathImgInput = ".\\immagini\\scenaTelefono3.jpg"
+pathImgOutput = ".\\immagini\\scenaTelefono5.jpg"
+pathModello = ".\\modelli_addestrati\\modelloDenso.keras"
+
+# Carico il modello pre-addestrato
 modello = load_model(pathModello)
 
+# Riconosco i numeri nelle immagini
 matriceInput = ric.riconosci_immagine(pathImgInput, modello)
 matriceOutput = ric.riconosci_immagine(pathImgOutput, modello)
 
+# Stampo le matrici per verificare il risultato
 print("Matrice di input:")
 print(matriceInput)
 print("Matrice di output:")
 print(matriceOutput)
 
+# Costruisco il problema
 problemone = problema.BlocksWorldProblem(problema.Board(matriceInput), problema.Board(matriceOutput))
+# Eseguo l'algoritmo A* per trovare il percorso ottimale
 problema.execute("Da scena 3 a 5", problema.aStar, problemone)
+
+# Creo la gif finale
